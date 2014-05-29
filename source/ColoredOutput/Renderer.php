b8b9beca40bc656a21b6f6cd8c21095ac19610b0
Ilya S <public.ilya.s@gmail.com>
2014-05-29 23:49:21 +0700

    add main method sign in Renderer (render)

--- source/ColoredOutput/Renderer.php
+++ source/ColoredOutput/Renderer.php
@@ -2,6 +2,17 @@
 
 class Renderer {
 
+    /**
+     * Render a message with given colors.
+     *
+     * @param string $message
+     * @param string|null $foreground
+     * @param string|null $background
+     * @return string
+     */
+    public function render($message, $foreground = null, $background = null)
+    {
 
+    }
 
 }

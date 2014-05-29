08c0bcd3481d9d241f96ca898c455e140db01618
Ilya S <public.ilya.s@gmail.com>
2014-05-30 00:05:45 +0700

    plan to store colors in properties, can be improved?

--- source/ColoredOutput/Renderer.php
+++ source/ColoredOutput/Renderer.php
@@ -3,6 +3,30 @@
 class Renderer {
 
     /**
+     * The foreground colors.
+     *
+     * @var array
+     */
+    protected $foreground = [
+        'black'        => '',
+        'dark_gray'    => '',
+        'blue'         => '',
+        'light_blue'   => '',
+        'green'        => '',
+        'light_green'  => '',
+        'cyan'         => '',
+        'light_cyan'   => '',
+        'red'          => '',
+        'light_red'    => '',
+        'purple'       => '',
+        'light_purple' => '',
+        'brown'        => '',
+        'yellow'       => '',
+        'light_gray'   => '',
+        'white'        => '',
+    ] ;
+
+    /**
      * Render a message with given colors.
      *
      * @param string $message

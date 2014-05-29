87c22c6aa0d4d550743715670e2220107cc3d4fc
Ilya S <public.ilya.s@gmail.com>
2014-05-29 23:59:22 +0700

    add a spec

--- specs/Spec/ColoredOutput/RendererSpec.php
+++ specs/Spec/ColoredOutput/RendererSpec.php
@@ -9,4 +9,10 @@ class RendererSpec extends ObjectBehavior {
         $this->shouldHaveType('ColoredOutput\Renderer');
     }
 
+    function it_renders_a_message()
+    {
+        $this->render('Hello, world!', 'red', 'green')
+             ->shouldBe('\033[0;31m\033[42mHello, world!\033[0m');
+    }
+
 }

6cafa768f0517c85eff672fecf3ef1ab2bc3db11
Ilya S <public.ilya.s@gmail.com>
2014-05-31 23:11:01 +0700

    add the actual color codes

--- source/ColoredOutput/Renderer.php
+++ source/ColoredOutput/Renderer.php
@@ -8,22 +8,22 @@ class Renderer {
      * @var array
      */
     protected $foreground = [
-        'black'        => '',
-        'dark_gray'    => '',
-        'blue'         => '',
-        'light_blue'   => '',
-        'green'        => '',
-        'light_green'  => '',
-        'cyan'         => '',
-        'light_cyan'   => '',
-        'red'          => '',
-        'light_red'    => '',
-        'purple'       => '',
-        'light_purple' => '',
-        'brown'        => '',
-        'yellow'       => '',
-        'light_gray'   => '',
-        'white'        => '',
+        'black'        => '0;30',
+        'dark_gray'    => '1;30',
+        'blue'         => '0;34',
+        'light_blue'   => '1;34',
+        'green'        => '0;32',
+        'light_green'  => '1;32',
+        'cyan'         => '0;36',
+        'light_cyan'   => '1;36',
+        'red'          => '0;31',
+        'light_red'    => '1;31',
+        'purple'       => '0;35',
+        'light_purple' => '1;35',
+        'brown'        => '0;33',
+        'yellow'       => '1;33',
+        'light_gray'   => '0;37',
+        'white'        => '1;37',
     ];
 
     /**
@@ -32,14 +32,14 @@ class Renderer {
      * @var array
      */
     protected $background = [
-        'black'      => '',
-        'red'        => '',
-        'green'      => '',
-        'yellow'     => '',
-        'blue'       => '',
-        'magenta'    => '',
-        'cyan'       => '',
-        'light_gray' => '',
+        'black'      => '40',
+        'red'        => '41',
+        'green'      => '42',
+        'yellow'     => '43',
+        'blue'       => '44',
+        'magenta'    => '45',
+        'cyan'       => '46',
+        'light_gray' => '47',
     ];
 
     /**
